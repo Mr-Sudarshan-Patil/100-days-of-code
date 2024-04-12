d676f3362b8dc6160b09be6284ce7b9117d85d08
Mr-Sudarshan-Patil <mr.sudarsh@gmail.com>
2024-04-12 21:58:34 +0530

    new changes

--- Node-Js/script.js
+++ Node-Js/script.js
@@ -4,8 +4,14 @@
 //     console.log("hello", i);
 // }
 
-let argv= process.argv;
+// let argv= process.argv;
 
-for(let i=0; i<argv.length; i++){
-    console.log("Arg", argv[i]);
-}
+// for(let i=0; i<argv.length; i++){
+//     console.log("Arg", argv[i]);
+// }
+
+
+const math = require("./math");
+
+console.log(math.sum(6,2));
+console.log(typeof math);
